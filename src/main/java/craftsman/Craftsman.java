d783260495d0a8c4ab713093654c99f61970f2b8
ththao27 <tranthanhthao2712@gmail.com>
2023-10-02 22:52:42 +0700

    test push code

---
+++ src/main/java/craftsman/Craftsman.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package craftsman;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Craftsman {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/data/Rectagle.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Rectagle {
+    private String owner;
+    private String color;
+    private double width;
+    private double length;
+
+    public Rectagle(String owner, String color, double width, double length) {
+        this.owner = owner;
+        this.color = color;
+        this.width = width;
+        this.length = length;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectagle{" + "owner=" + owner + ", color=" + color + ", width=" + width + ", length=" + length + '}';
+    }
+ 
+    public void paint() {
+        
+    }
+    
+}
